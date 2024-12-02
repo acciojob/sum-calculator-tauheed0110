8faa22b1b6776ca92a4e41e8bc7dc6753b8f0941
saif0110 <khanmohdtauheed4@gmail.com>
2024-12-02 13:54:11 +0530

    file changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [value, setValue] = useState("");
+  const [sum , setSum] = useState("");
+
+  useEffect(()=>{
+    setSum(prev => {
+      return Number(prev) + Number(value);
+    });
+  }, [value]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Sum Calculator</h1>
+        <input type="number" value={value} onChange={(e)=> setValue(e.target.value)}/>
+        <p>Sum: {sum}</p>
     </div>
   )
 }
